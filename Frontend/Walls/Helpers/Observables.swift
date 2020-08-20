1c2dd95720fa17090c7d9b1925489e25a3289701
Nour Yehia <noureldine.yehia@gmail.com>
2020-08-20 12:24:28 -0700

    File containing obersvables to be used

---
+++ Frontend/Walls/Helpers/Observables.swift
@@ -0,0 +1,13 @@
+//
+//  Observables.swift
+//  Walls
+//
+//  Created by Nour Yehia on 8/20/20.
+//  Copyright © 2020 Nour Yehia. All rights reserved.
+//
+
+import Foundation
+
+class Id: ObservableObject {
+    @Published var id: Int = 0
+}
